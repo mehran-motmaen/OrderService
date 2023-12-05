66826a39e481b7b1aa48d1406a592c83e9f5223e
motmaen <M5566M>
2023-12-05 14:08:02 +0330

    use other container

--- order_service/orders/views.py
+++ order_service/orders/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import json
 import pika
-
+import requests
 
 class OrderCreateView(generics.CreateAPIView):
     queryset = Order.objects.all()
@@ -14,6 +14,31 @@ class OrderCreateView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
+        # Fetch customer_fullname from user-service
+        user_id = serializer.validated_data['user_id']
+        user_service_url = f'http://user-service:8080/users/{user_id}'
+        user_response = requests.get(user_service_url)
+
+        if user_response.status_code == 200:
+            user_data = user_response.json()
+            serializer.validated_data['customer_fullname'] = f"{user_data.get('firstName', '')} {user_data.get('lastName', '')}"
+        else:
+            return Response({"error": "Unable to fetch user information"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        # Fetch product_name and total_amount from product-service
+        product_code = serializer.validated_data['product_code']
+        product_service_url = f'http://product-service:8080/products/{product_code}'
+        product_response = requests.get(product_service_url)
+        if product_response.status_code == 200:
+            product_data = product_response.json()
+            serializer.validated_data['product_name'] = product_data.get('name', '')
+            serializer.validated_data['total_amount'] = product_data.get('price', 0.0)
+        else:
+            return Response({"error": "Unable to fetch product information"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
 
